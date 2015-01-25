35da4adb49c725d09451a62a85e8820cb7ab746f
SambitAcharya <sambitacharya121@gmail.com>
2015-01-25 21:02:01 +0530

    added code for sanitizing input file.

--- Desktop App/texttospeech.py
+++ Desktop App/texttospeech.py
@@ -2,14 +2,24 @@ import os
 import sys
 import urllib2
 
-def downloadMp3File():
-    mp3file = urllib2.urlopen("http://www.example.com/songs/mp3.mp3")
+def downloadMp3File(url):
+    # mp3file = urllib2.urlopen("http://www.example.com/songs/mp3.mp3")
+    mp3file = urllib2.urlopen(url)
     output = open('test.mp3','wb')
     output.write(mp3file.read())
     output.close()
+
+def changeText(text):
+    text = text.replace('\n','')
+    text_list = re.split('(\,|\.|\;|\:)', text)
+
+    lines = []
+
 def main():
     file_name = raw_input("Enter the name of the file.\n")
-    file = open(file_name,'r').read()
-    print(file)
+    file = open(file_name,'r')
+    text = file.read()
+    # print(text)
+    changeText(text)
 
 main()
