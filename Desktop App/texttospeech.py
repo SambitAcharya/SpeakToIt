af130fe4004243c8b3bc184fcf198d003eaf82d2
SambitAcharya <sambitacharya121@gmail.com>
2015-01-25 20:28:50 +0530

    Added code to read file and download mp3 file.

--- Desktop App/texttospeech.py
+++ Desktop App/texttospeech.py
@@ -2,10 +2,14 @@ import os
 import sys
 import urllib2
 
-def downloadAudio():
-    return 0
-
+def downloadMp3File():
+    mp3file = urllib2.urlopen("http://www.example.com/songs/mp3.mp3")
+    output = open('test.mp3','wb')
+    output.write(mp3file.read())
+    output.close()
 def main():
-    return 0
+    file_name = raw_input("Enter the name of the file.\n")
+    file = open(file_name,'r').read()
+    print(file)
 
 main()
