7b33d8aaf2bc290767a1de49b16cae9881ed9ad6
SambitAcharya <sambitacharya121@gmail.com>
2015-02-11 20:50:46 +0530

    Updated script code.

---
+++ Web App/Text-To-Speech/js/script.js
@@ -0,0 +1,8 @@
+//Populate voice selection dropdown
+var voicelist = responsiveVoice.getVoices();
+
+var vselect = $("#voiceselection");
+
+$.each(voicelist, function() {
+        vselect.append($("<option />").val(this.name).text(this.name));
+});
